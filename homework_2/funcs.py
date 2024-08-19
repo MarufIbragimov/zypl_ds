5f38a5d0c6be26a92636db3c47c8c0bfa9ae56e2
maruf ibragimov <ibragimov.maruf@gmail.com>
2024-08-19 19:35:55 +0500

    add plot_auc_per_category function

--- homework_2/funcs.py
+++ homework_2/funcs.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -80,3 +81,67 @@ def plot_feature_importance(model, feature_names):
     plt.title('Feature Importance Plot')
 
     plt.show()
+
+def compute_auc_per_category(model, X_train, y_train, X_test, y_test, cat_feature):
+    """
+    Считает ROC_AUC по указанной категории.
+
+    Принимает:
+        * model - модель
+        * X_train - тренировочный сет предикторов
+        * y_train - тренировочный сет целевой переменной
+        * X_test - тестовый сет предикторов
+        * y_test - тестовый сет целевой переменной
+        * cat_feature - название категориального предиктора 
+
+    Возвращает:
+        * список, содержащий:
+            - category - название категории
+            - train_count - количество наблюдений в тренировочном сете
+            - train_auc - ROC_AUC тренировочного сета
+            - test_count - количество наблюдений в тестовом сете
+            - test_auc - ROC_AUC тестового сета
+    """
+    unique_categories = X_train[cat_feature].unique()
+    results = []
+
+    for category in unique_categories:
+        train_indices = X_train[cat_feature] == category
+        test_indices = X_test[cat_feature] == category
+        
+        train_count = np.sum(train_indices)
+        test_count = np.sum(test_indices)
+        
+        try:
+            train_auc = metrics.roc_auc_score(y_train[train_indices], model.predict(X_train[train_indices]))
+            test_auc = metrics.roc_auc_score(y_test[test_indices], model.predict(X_test[test_indices]))
+        except ValueError:
+            train_auc = np.nan
+            test_auc = np.nan
+
+        results.append([category, train_count, train_auc, test_count, test_auc])
+
+    return results
+
+def plot_auc_per_category(model, X_train, y_train, X_test, y_test, cat_features):
+    """
+    Выводит на экран таблицу сравнений метрики ROC_AUC по категориям.
+
+    Принимает:
+        * model - модель
+        * X_train - тренировочный сет предикторов
+        * y_train - тренировочный сет целевой переменной
+        * X_test - тестовый сет предикторов
+        * y_test - тестовый сет целевой переменной
+        * cat_features - список категориального предикторов
+    """
+    for feature in cat_features:
+        results = compute_auc_per_category(model, X_train, y_train, X_test, y_test, feature)
+        df_results = pd.DataFrame(results, columns=['Category', 'Train Count', 'Train AUC', 'Test Count', 'Test AUC'])
+        
+        df_results['AUC Difference'] = df_results['Train AUC'] - df_results['Test AUC']
+        df_results_sorted = df_results.sort_values(by='AUC Difference', ascending=False)
+        
+        print(f"\nAUC and Counts for {feature} sorted by AUC difference:\n")
+        display(df_results_sorted.drop(['AUC Difference'], axis=1))
+        print("\n" + "-" * 50 + "\n")
